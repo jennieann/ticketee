dc47042e24bbef82cf123bdbc3454640b92ed95c
Jennie Sörnell <mail@jennie-ann.se>
2017-01-05 14:41:32 +0100

    Ticketee: Added Devise signup feature; users can sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+  devise_for :users
 end
