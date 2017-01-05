d77aae30515af5abd805a38a2f7ea25486578e1b
Jennie Sörnell <mail@jennie-ann.se>
2017-01-05 11:48:22 +0100

    Ticketee: Tickets can be deleted

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -32,6 +32,13 @@ class TicketsController < ApplicationController
     end
   end
 
+  def destroy
+    @ticket.destroy
+
+    flash[:alert] = "Ticket has been deleted"
+    redirect_to @project
+  end
+
   private
   def ticket_params
     params.require(:ticket).permit(:name, :description)
